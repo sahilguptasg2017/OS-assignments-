0dfb733ad8464d5f49297ff84c894d283bd6c645
sahilguptasg2017 <sahilguptasg2017@gmail.com>
2023-08-22 01:04:48 +0530

    working

--- operating_system/Assignment-1/Question3/Calculator.sh
+++ operating_system/Assignment-1/Question3/Calculator.sh
@@ -6,6 +6,7 @@ out_file="$out_directory/output.txt"
 
 mkdir -p "$out_directory"
 
+
 while IFS= read -r line || [ -n "$line" ]; do
     if [ -n "$line" ]; then
         x=$(echo "$line" | awk '{print $1}')
@@ -20,10 +21,12 @@ while IFS= read -r line || [ -n "$line" ]; do
             echo "$result" >> "$out_file"
         else
             if [ "$x" -gt "$y" ]; then
-                echo "$x" >> "$out_file"
+                echo "$x" >> "$out_file
             else 
                 echo "$y" >> "$out_file"
             fi
         fi
     fi
 done < "/mnt/c/Sahil/Python/Sahil/operating_system/Assignment-1/Question3/input.txt"
+
+#working 
